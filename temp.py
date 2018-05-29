bbe35c2127549644d0973d579c8e201579e64145
TanjillaTina <tanjilla.tina@gmail.com>
2018-05-29 12:10:26 -0700

    Initial Commit

---
+++ temp.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+"""
+In The Name of Allah The Beneficent and The Merciful
+Spyder Editor
+
+This is a temporary script file.
+"""
+import nltk
+nltk.download()
+'''
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+example_text="Hello, Mr.Anaconda, How Are you?? I will be Glad, If you help me to perform my Natural Language Practice."
+
+
+print(sent_tokenize(example_text))  #Sentence Tokenizer
+print(word_tokenize(example_text))  #Word Tokenizer
+
+for i in word_tokenize(example_text):
+    print(i)
+'''
